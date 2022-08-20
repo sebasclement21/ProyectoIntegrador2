bd72ad1a64bbb50e94d146a6f3b5b9d4af256f22
Sebastian Clement <sebasclement.sc@gmail.com>
2022-08-20 19:43:44 -0300

    Cambios en el directorio raiz

---
+++ ArgentinaPrograma/BackEnd/PIAP/src/main/java/com/portfolioSC/PIAP/PiapApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioSC.PIAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiapApplication.class, args);
+	}
+
+}
---
+++ ArgentinaPrograma/BackEnd/PIAP/src/test/java/com/portfolioSC/PIAP/PiapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioSC.PIAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
